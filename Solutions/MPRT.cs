d8f66de37d57f6dfc8f7ab66d4663a0cc8143236
Robi <34911342+xtremealu@users.noreply.github.com>
2020-10-04 18:26:30 +0200

    Add no RegEx solution for MPRT

--- Solutions/MPRT.cs
+++ Solutions/MPRT.cs
@@ -45,5 +45,48 @@ namespace Rosalind.Solutions
             File.WriteAllLines(@"..\..\..\Output\mprt.txt", output);
             return @"Output written to Output\mprt.txt";
         }
+
+        public static string GetProteinLocationsNoRegex() // Because the solution above is kinda overkill, LMAO. I like to overthink, do I? I've been doing RegEx recently so I guess that's why. They execute in similar time, this one even slower on my laptop.
+        {
+            // Write input file with protein IDs to string array
+            string[] input = File.ReadAllLines(@"..\..\..\Datasets\rosalind_mprt.txt");
+            List<string> outputList = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Parse current protein from input to dictionary with key (protein ID) and value (sequence)
+                string[] proteinData = Library.UniprotDownload(input[i]);
+                Dictionary<string, string> temp = Library.ParseFastaToDictionary(proteinData);
+
+                string protein = temp.ElementAt(0).Value; // Protein sequence
+                string identifier = temp.ElementAt(0).Key; // Protein identifier
+
+                // Match the motif N{P}[ST]{P} without RegEx
+                bool hasIdentifier = false;
+                string locations = "";
+                for (int j = 0; j < protein.Length - 3; j++)
+                {
+                    if (protein[j] == 'N' && protein[j + 1] != 'P' && (protein[j + 2] == 'S' || protein[j + 2] == 'T') && protein[j + 3] != 'P')
+                    {
+                        if (!hasIdentifier)
+                        {
+                            outputList.Add(identifier);
+                            hasIdentifier = true;
+                        }
+                        locations += (j + 1).ToString() + " ";
+                    }
+                }
+                if (hasIdentifier)
+                {
+                    locations = locations.Trim();
+                    outputList.Add(locations);
+                }
+            }
+
+            // Convert list with motif locations to array for file writing
+            string[] output = outputList.ToArray();
+            File.WriteAllLines(@"..\..\..\Output\mprt.txt", output);
+            return @"Output written to Output\mprt.txt";
+        }
     }
 }
